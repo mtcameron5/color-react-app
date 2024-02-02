a172ed1847eda2e86d8d19a916b10da04c2885c0
mtcameron5 <mtcameron5@gmail.com>
2024-02-02 06:06:33 -0700

    first commit

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        {/* Navbar goes here */}
+        <div className='Palette-colors'>
+
+        </div>
+        {/* footer eventually */}
+        <h1>PALETTE</h1>
+      </div>
+    )
+  }
+}
+
